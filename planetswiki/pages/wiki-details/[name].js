b3d74b5b855c41aa06798e764a8aece7c52bb120
Jesus Castro <tony@Jesuss-MacBook-Pro.local>
2020-08-31 16:49:44 -0700

    Fixed wiki page on close

--- planetswiki/pages/wiki-details/[name].js
+++ planetswiki/pages/wiki-details/[name].js
@@ -15,8 +15,8 @@ function useEntity(name) {
 
 function WikiDetails() {
     const [pageTracker, setPageTracker] = useState(0);
-    const { query } = useRouter();
-    const { name } = query;
+    const router = useRouter();
+    const { name } = router.query;
     const { entityList, isError, isLoading } = useEntity(name);
 
 
@@ -26,7 +26,9 @@ function WikiDetails() {
         <video className="wiki-details__media-video">
             <source src={video.href} />
         </video>
-    )
+    );
+
+    const close = () => router.push('/')
 
     const back = () => {
         const tracker = (pageTracker === 0) ? 0 : pageTracker - 1;
@@ -49,15 +51,18 @@ function WikiDetails() {
 
     return (
         <Fragment>
-            <div className="wiki-details__media">
+            <div className="wiki-details__close">
+                <button onClick={close}>X</button>
+            </div>
+            <section className="wiki-details__media">
                 {media.render === 'image' && renderImage(media)}
                 {media.render === 'image' && renderVideo(media)}
-            </div>
-            <div className="wiki-details">
+            </section>
+            <section className="wiki-details">
                 <h2 className="wiki-details__title">{details.title}</h2>
                 <p className="wiki-details__date">{details.date_created}</p>
                 <p className="wiki-details__description">{details.description}</p>
-            </div>
+            </section>
             <NavBar back={back} next={next} tracker={pageTracker} />
         </Fragment>
     )
